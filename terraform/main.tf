8f55df78e5258aa029a4a5030adf8b283757ebf6
stefanidmtrv <stefanidmtrv@DESKTOP-NNCJM1D.>
2025-08-28 11:51:30 +0300

    Add required provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
 provider "aws" {
   region = "eu-north-1"
 }
