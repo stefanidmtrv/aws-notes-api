bdeb618a846795433dcffa9a80388b9fd8fd9cad
stefanidmtrv <stefanidmtrv@DESKTOP-NNCJM1D.>
2025-08-28 12:18:45 +0300

    Change hashicorp version constraint

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,10 +3,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
-  required_version = ">= 1.1.0"
 }
 
 provider "aws" {
